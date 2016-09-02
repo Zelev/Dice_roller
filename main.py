9b9dab6bfb11651971aedb4b03a386f3b5cd686c
Zelev <danielvelezm@gmail.com>
2016-09-02 09:23:33 -0500

    Add Regex for Strings and Effect Roll
    * Added Regex rules for the input strings so the user can check
    what he did wrong
    * Added the Effect Roll capablity, so the user can roll n dice of
    n sides and add a positive or negative modifier and get the final
    sum of the resulting rolls and the modifier.
    
    The Effect Roll will get inputs of the: #d#(+|-)# allowing
    whiespaces between numbers and the other chars

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random 
+import random
+import re
 
 
-def roll(die, times):
+def simple_roll(die, times):
     '''
     list[die]: list with the numbers that will display the die
     int[times]: number of rolls to perform in the given list
@@ -13,12 +14,32 @@ def roll(die, times):
     return result
 
 
+def effect_roll(str_input):
+    values = re.findall(r'\d*', str_input)
+    values = [x for x in values if x != '']
+    dice_roll = simple_roll(xrange(1, int(values[1]) + 1), int(values[0]))
+    result = sum(dice_roll)
+    if '+' in str_input:
+        result += int(values[-1])
+    elif '-' in str_input:
+        result -= int(values[-1])
+    return result, dice_roll
+
+
 def main():
+    '''This regex will give the pattern for the rolls'''
+    effect_roll_rx = re.compile('^\d+\s*[d|D]{1}\s*\d+\s*([+]?|[-]?)\s*\d+$')
+    simple_roll_rx = re.compile('^\d+\s*[d|D]{1}\s*\d+$')
     while True:
         raw_roll = raw_input('What you want to roll?  ')
-        to_roll = raw_roll.split('d')
-        print roll(xrange(1, int(to_roll[1]) + 1),
-                   int(to_roll[0]))
+        if effect_roll_rx.match(raw_roll):
+            print effect_roll(raw_roll)
+        elif simple_roll_rx.match(raw_roll):
+            to_roll = raw_roll.split('d')
+            print simple_roll(xrange(1, int(to_roll[1]) + 1),
+                              int(to_roll[0]))
+        else:
+            print "There was an error with the input, please check:  " + raw_roll
 
 
 main()
